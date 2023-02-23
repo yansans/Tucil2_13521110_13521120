5617e9251ffdc59a427f096ef65952b402b42a68
yansans <66671259+yansans@users.noreply.github.com>
2023-02-23 15:43:15 +0700

    add type hint

--- src/point.py
+++ src/point.py
@@ -2,11 +2,11 @@ import math
 import random
 
 class Point:
-    def __init__(self, *coords):
+    def __init__(self, *coords: list):
         self.coords = coords
         self.dimension = len(coords)
 
-    def euclideanDistance(self, q):
+    def euclideanDistance(self, q: 'Point') -> float:
         if self.dimension != q.dimension:
             raise ValueError("Point have different dimensions!")
         return math.sqrt(sum((self.coords[i] - q.coords[i])**2 
@@ -17,25 +17,25 @@ class Points:
         self.points = list(points)
         self.size = len(self.points)
 
-    def add_point(self, point):
+    def add_point(self, point: Point):
         self.points.append(point)
         self.size += 1
 
-    def remove_point(self, index):
+    def remove_point(self, index: int):
         self.points.pop(index)
         self.size -= 1
 
-    def get_point(self, index):
+    def get_point(self, index) -> Point:
         return self.points[index]
 
-    def distance(self, i, j):
+    def distance(self, i: int, j: int) -> float:
         return self.points[i].euclideanDistance(self.points[j])
     
     def print_points(self):
         for i in range(self.size):
             print(f"{i+1}." , self.points[i].coords)
     
-def generate_points(n, dimension, lim = 100):
+def generate_points(n: int, dimension: int, lim: int = 100) -> Points:
     points = Points()
     part = lim // n
     below = -lim
