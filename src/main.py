f16553dd6eb82a8993dbaab492b0974577f88a91
yansans <66671259+yansans@users.noreply.github.com>
2023-02-28 19:23:34 +0700

    refactor main, add animation

--- src/main.py
+++ src/main.py
@@ -4,8 +4,33 @@ from plotPoint import *
 import globals
 import time
 
-def nearest_neighbour(points: list, key: str, sort: int = 1) -> tuple:
-    globals.initialize(d)
+def main():
+
+    d = menu(1)
+    n_point = menu(2)
+    rounding = menu(3)
+    # 1 Merge sort , 2 Quick sort
+    sort = menu(4)
+
+    bound = 10**3
+
+    points = random_point(n_point , d, rounding, bound)
+
+    print("==================================================================================")
+    
+    nn_dnc , nnDist_dnc = nearest_point(points, d, "dnc", sort)
+
+    print()
+
+    nn_b , nnDist_b = nearest_point(points, d, "brute")
+
+    print("==================================================================================")
+    visual = input("Do you want to visualize the result? (y/n): ")
+    if visual == "y":
+        plotPoint(points, nn_dnc, d)
+
+def nearest_point(points: list, dim:int, key: str, sort:int = 1) -> tuple:
+    globals.initialize(dim)
     if key == "dnc":
         print(f"Result with divide and conquer:")
     elif key == "brute":
@@ -23,59 +48,58 @@ def nearest_neighbour(points: list, key: str, sort: int = 1) -> tuple:
     print(f"Number of euclidian operation: {globals.count}")
     return nn, nnDist
 
-
-if __name__ == '__main__':
-    
+def menu(type : int) -> int:
+    if type == 1:
+        prompt = "Enter the dimension of the points: "
+        minval = 2
+        err = "Dimension must be greater than 0"
+    elif type == 2:
+        prompt = "Enter the number of points: "
+        minval = 1
+        err = "Number of points must be greater than 0"
+    elif type == 3:
+        prompt = "Enter the rounding of the points: "
+        minval = 0
+        err = "Rounding must be greater than 0"
+    elif type == 4:
+        prompt = "Enter the sorting method\n1. Merge sort \n2. Quick sort\n :"
+        minval = 1
+        err = "Sorting method must be 1 or 2"
     while True:
-        try:
-            d = int(input("Enter dimension: "))
-            if d < 1:
-                print("Dimension must be greater than 0")
+            try:
+                num = int(input(prompt))
+                if num < minval:
+                    print(err)
+                    continue
+                break
+            except ValueError:
+                print("Invalid input")
                 continue
-            break
-        except:
-            print("Invalid input")
+    return num
 
-    while True:
-        try:
-            n_point = int(input("Enter number of point: "))
-            if n_point < 2:
-                print("Number of point must be greater than 1")
-                continue
-            break
-        except:
-            print("Invalid input")
 
-    while True:
-        try:
-            rounding = int(input("Enter rounding (ketelitian angka di belakang koma): "))
-            if rounding < 0:
-                print("Rounding must be greater than 0")
-                continue
-            break
-        except:
-            print("Invalid input")
-    
-    # 1 merge sort
-    # 2 quick sort
-    # else python sort
-    while True:
-        try:
-            sort = int(input("Enter sorting method (1: merge sort, 2: quick sort, else python sort): "))
-            break
-        except:
-            print("Invalid input")
+def animation():
+    animation = [
+    "[        ]",
+    "[=       ]",
+    "[==      ]",
+    "[===     ]",
+    "[====    ]",
+    "[=====   ]",
+    "[======  ]",
+    "[======= ]",
+    "[========]",
+    ]
+    i = 0
+    for i in range(len(animation)):
+        print(animation[i % len(animation)], end='\r')
+        time.sleep(0.1)
 
-    print("==================================================================================")
-    points = random_point(n_point ,d, rounding, 10)
-    
-    nn_dnc , nnDist_dnc = nearest_neighbour(points, "dnc", sort)
+def startscreen():
+    animation()
+    print("Welcome to the nearest point program")
+    input("Press enter to continue...")
 
-    print()
-
-    nn_b , nnDist_b = nearest_neighbour(points, "brute")
-
-    print("==================================================================================")
-    visual = input("Do you want to visualize the result? (y/n): ")
-    if visual == "y":
-        plotPoint(points, nn_dnc, d)
+if __name__ == "__main__":
+    startscreen()
+    main()
